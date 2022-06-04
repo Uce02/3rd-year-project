7cb5d26d08734c047f615745cde06e84444b43c7
Uce02 <c0028383@newcastle.ac.uk>
2022-06-04 17:48:48 +0100

    Added dialogue text and battle text
    various functions and calls made to allow for the moves, PP and type of the moves to be displayed and cycled through by the player

---
+++ 3rdYearProject/Assets/Scripts/Battle/BattleDialogueBox.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleDialogueBox : MonoBehaviour
+{
+  [SerializeField] int letterPerSecond;
+  [SerializeField] Color highlightedColour;
+
+  [SerializeField] Text dialogueText;
+  [SerializeField] GameObject actionSelector;
+  [SerializeField] GameObject moveSelector;
+  [SerializeField] GameObject moveDetails;
+
+  [SerializeField] List<Text> actionTexts;
+  [SerializeField] List<Text> moveTexts;
+
+  [SerializeField] Text ppText;
+  [SerializeField] Text typeText;
+
+  public void SetDialogue(string dialogue)
+  {
+    dialogueText.text = dialogue;
+  }
+
+  public IEnumerator TypeDialogue(string dialogue)
+  {
+    dialogueText.text = "";
+    // loop through each letter and them one by one to make the dialogue scroll along
+    foreach (var letter in dialogue.ToCharArray())
+    {
+      dialogueText.text += letter;
+      // wait for 1/30th of a second after displaying each letter (30 letters in one second)
+      yield return new WaitForSeconds(1f/letterPerSecond);
+    }
+  }
+
+  public void EnableDialougeText(bool enabled)
+  {
+    dialogueText.enabled = enabled;
+  }
+
+  public void EnableActionSelector(bool enabled)
+  {
+    actionSelector.SetActive(enabled);
+  }
+
+  public void EnableMoveSelector(bool enabled)
+  {
+    moveSelector.SetActive(enabled);
+    moveDetails.SetActive(enabled);
+  }
+
+  public void UpdateActionSelection(int selectedAction)
+  {
+    // loop through action texts and if current action is selected change colour
+    for (int i=0; i<actionTexts.Count; ++i)
+    {
+      if (i == selectedAction)
+          actionTexts[i].color = highlightedColour;
+      else
+          actionTexts[i].color = Color.black;
+    }
+  }
+
+  public void UpdateMoveSelection(int selectedMove, Move move)
+  {
+    // loop through move texts and if current move is selected change colour
+    for (int i=0; i<moveTexts.Count; ++i)
+    {
+      if (i == selectedMove)
+          moveTexts[i].color = highlightedColour;
+      else
+          moveTexts[i].color = Color.black;
+    }
+    // show current PP of the move and base PP (how many moves you have left out of total times it can be used)
+    ppText.text = $"PP {move.PP}/{move.Base.PP}";
+    // show type of current move
+    typeText.text = move.Base.Type.ToString();
+  }
+
+  public void SetMoveNames(List<Move> moves)
+  {
+    for (int i=0; i<moveTexts.Count; ++i)
+    {
+      if (i < moves.Count)
+          moveTexts[i].text = moves[i].Base.Name;
+      else
+      // if all moves of the animal have been displayed  and there is still an empty space put this in its place
+          moveTexts[i].text = "-";
+    }
+  }
+}
--- 3rdYearProject/Assets/Scripts/Battle/BattleSystem.cs
+++ 3rdYearProject/Assets/Scripts/Battle/BattleSystem.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//store the state of battle
+
+public enum BattleState { Start, PlayerAction, PlayerMove, EnemyMove, Busy}
+
 public class BattleSystem : MonoBehaviour
 {
   // control the entire Battle
@@ -9,13 +13,19 @@ public class BattleSystem : MonoBehaviour
   [SerializeField] BattleUnit enemyUnit;
   [SerializeField] BattleHUD playerHUD;
   [SerializeField] BattleHUD enemyHUD;
+  [SerializeField] BattleDialogueBox dialogueBox;
+
+  BattleState state;
+  // variables to store current action and current move
+  int currentAction;
+  int currentMove;
 
   private void Start()
   {
-    SetupBattle();
+    StartCoroutine(SetupBattle());
   }
 
-  public void SetupBattle()
+  public IEnumerator SetupBattle()
   {
     //calls on playerUnit from BattleUnit to show the animal
     playerUnit.Setup();
@@ -23,5 +33,101 @@ public class BattleSystem : MonoBehaviour
     //set the HUD data of the animal called in the previous line from BattleUnit
     playerHUD.SetData(playerUnit.Animal);
     enemyHUD.SetData(enemyUnit.Animal);
+    // passing moves of current animal to the SetMoveNames function
+    dialogueBox.SetMoveNames(playerUnit.Animal.Moves);
+
+    yield return dialogueBox.TypeDialogue($"A wild {enemyUnit.Animal.Base.Name} appeared!");
+    yield return new WaitForSeconds(1f);
+
+    PlayerAction();
+  }
+
+  void PlayerAction()
+  {
+    state = BattleState.PlayerAction;
+    StartCoroutine(dialogueBox.TypeDialogue("Choose an action"));
+    dialogueBox.EnableActionSelector(true);
+  }
+
+  void PlayerMove()
+  {
+    state = BattleState.PlayerMove;
+    dialogueBox.EnableActionSelector(false);
+    dialogueBox.EnableDialougeText(false);
+    dialogueBox.EnableMoveSelector(true);
+    // only show move selector, disable any other text from showing up in the dialogue box
+  }
+
+  private void Update()
+  {
+    if (state == BattleState.PlayerAction)
+    {
+      HandleActionSelection();
+    }
+    else if (state == BattleState.PlayerMove)
+    {
+      HandleMoveSelection();
+    }
+  }
+
+  void HandleActionSelection()
+  {
+    if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
+      // if on 0 (fight) and you press down, you're now on 1 and vice versa
+      if (currentAction < 1)
+          ++currentAction;
+    }
+    else if (Input.GetKeyDown(KeyCode.UpArrow))
+    {
+      if (currentAction > 0)
+          --currentAction;
+    }
+
+    dialogueBox.UpdateActionSelection(currentAction);
+
+    if (Input.GetKeyDown(KeyCode.Z))
+    {
+      if (currentAction == 0)
+      {
+        // Fight. if clicked the player will be asked to select a move
+        PlayerMove();
+      }
+      else if (currentAction == 1)
+      {
+        // Run
+      }
+    }
+  }
+
+  void HandleMoveSelection()
+  {
+    // update current move based on user input, like above HandleActionSelection function
+    if (Input.GetKeyDown(KeyCode.RightArrow))
+    {
+      // one to the right is move 1
+      if (currentMove < playerUnit.Animal.Moves.Count - 1)
+          ++currentMove;
+    }
+    else if (Input.GetKeyDown(KeyCode.LeftArrow))
+    {
+      // first move is move 0
+      if (currentMove > 0)
+          --currentMove;
+    }
+    else if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
+      // first down and left is move 3
+      if (currentMove < playerUnit.Animal.Moves.Count -2)
+          currentMove += 2;
+    }
+    else if (Input.GetKeyDown(KeyCode.UpArrow))
+    {
+      // down and to the right is move 4
+      if (currentMove > 1)
+          currentMove -= 2;
+    }
+
+    dialogueBox.UpdateMoveSelection(currentMove, playerUnit.Animal.Moves[currentMove]);
   }
 }
